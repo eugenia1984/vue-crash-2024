bce7715bc550bb6098c4c6a3cd0291d8c19a6901
eugenia1984 <costamariaeugenia1@gmail.com>
2024-09-11 15:31:35 +0200

    feat: add loading spinner while fetching jobs

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -2,10 +2,11 @@
   import { RouterLink } from 'vue-router';
   import { reactive, defineProps, onMounted } from 'vue';
   import  JobListing  from '@/components/JobListing.vue';
+  import PulseLoader from 'vue-spinner/src/PulseLoader.vue';
   import axios from 'axios';
 
   defineProps({
-    limit: Number,
+    limit: Number, 
     showButton: {
       type: Boolean,
       default: false
@@ -35,7 +36,12 @@
       <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
         Browse jobs
       </h2>
-      <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+      <!-- Show Loading spinner while loading is true -->
+       <div v-if="state.isLoading" class="text-center text-gray-500 py-6">
+        <PulseLoader />
+       </div>
+       <!-- Show job listing when downloading -->
+      <div v-else class="grid grid-cols-1 md:grid-cols-3 gap-6">
         <JobListing v-for="job in state.jobs.slice(0, limit || state.jobs.length)" :key="job.id" :job="job"/>
       </div>
     </div>
